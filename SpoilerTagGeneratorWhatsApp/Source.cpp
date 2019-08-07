7cea51f5f39cb27b85c4075a8cb0be5be2298320
Tdead1 <tomkemna@gmail.com>
2019-08-07 19:56:39 +0200

    CHANGE Added a small spoiler tag generator for whatsapp.

---
+++ SpoilerTagGeneratorWhatsApp/Source.cpp
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <iostream>
+
+int main()
+{
+	std::wstring s = L"[SPOILER AHEAD]\n";
+	wchar_t wc = L'\u200Bc';
+
+	for (int i = 0; i < 4000; i++)
+		s = s + wc;
+
+	const wchar_t* cwc = s.data();
+	const size_t len = wcslen(cwc) + 1;
+
+	HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+	memcpy(GlobalLock(hMem), cwc, len);
+	GlobalUnlock(hMem);
+	OpenClipboard(0);
+	EmptyClipboard();
+	SetClipboardData(CF_UNICODETEXT, hMem);
+	CloseClipboard();
+
+	return 0;
+}
