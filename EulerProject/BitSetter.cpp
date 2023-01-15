d6f79a83b882e8a7444d4c98937a70b94f4d48a6
Tdead1 <32958884+Tdead1@users.noreply.github.com>
2023-01-15 21:49:31 +0100

    Added test case

--- EulerProject/BitSetter.cpp
+++ EulerProject/BitSetter.cpp
@@ -34,3 +34,22 @@ private:
 	static constexpr int ReservedSpace = static_cast<int>((TemplateSize + 7) / 8);
 	Byte myMemory[ReservedSpace];
 };
+
+#include <iostream>
+int main()
+{
+	constexpr int arraySize = 250;
+	BitArray<arraySize> bitArray1;
+	std::cout << " bit 0 is currently: " << bitArray1.GetBit(40) << std::endl;
+	std::cout << "byte reserved space: " << bitArray1.GetReservedBytes() << std::endl;
+	std::cout << " bit reserved space: " << bitArray1.GetReservedBits() << std::endl;
+	std::cout << " full class size is: " << sizeof(BitArray<7>) << std::endl;
+
+	for (int i = 0; i < bitArray1.GetReservedBits(); i++)
+	{
+		std::cout << bitArray1.GetBit(i) << "    ";
+		if ((i + 1) % 8 == 0)
+			std::cout << "\n";
+	}
+	return 0;
+}
